0ad31639247127ef7d06b4dadbeaf0ab34fa4e37
Cedric Blanchard <blanchard.ced@gmail.com>
2023-11-18 19:31:39 +0100

    update arenaWidget file with comments and docstrings

--- src/gui/arenaWidget.py
+++ src/gui/arenaWidget.py
@@ -14,14 +14,16 @@ CURR_DIR = Path(__file__).resolve().parent
 
 # TILE CLASS, SUBELEMENT OF THE ARENA GRID CLASS
 class Tile(QWidget):
-    def __init__(self, name:str, sideSize:int, parent: QWidget | None = ...):
+    """
+    Class defining the tile composing the arena map
+    """
+    def __init__(self, name:str, sideSize:int, parent: QWidget | None = ...) -> None:
         super().__init__(parent)
-
         self.objectName = name
         self.setAutoFillBackground(True)
         self.setFixedSize(sideSize, sideSize)
 
-    def setColor(self, r:int, g:int, b:int, a:int = 255):
+    def setColor(self, r:int, g:int, b:int, a:int = 255) -> None:
         """
         Define tile color based on rgb values
         """
@@ -43,7 +45,13 @@ class Tile(QWidget):
 
 # ARENA GRID CLASS TO MANAGE PAINTED TILES BY TEAMS
 class ArenaGrid(QWidget):
+    """
+    Class creating a grid for the arena. It uses the tile class to populate the grid, based on a number of rows and columns
+    """
     def __init__(self, row:int, col:int, widget:QWidget, sideSize:int, parent: QWidget | None = ...) -> None:
+        """
+        The init method set the grid, reset margins and adjust the widget size to exactly fit the grid
+        """
         super().__init__()
 
         self.objectName = 'arenaGrid'
@@ -58,7 +66,7 @@ class ArenaGrid(QWidget):
         self._layout.setContentsMargins(0,0, 0, 0)
         self.adjustSize() # Redraw the widget to fit size with all tiles
     
-    def setGrid(self, row:int, col:int, widget:QWidget, sideSize:int):
+    def setGrid(self, row:int, col:int, widget:QWidget, sideSize:int) -> None:
         """
         Set arena grid with inputs rows and columns number, and populate the grid with the specified widget
         """
@@ -68,12 +76,18 @@ class ArenaGrid(QWidget):
                 tile.setColor(255, 255, 255, 0)
                 self._layout.addWidget(tile, i, j)
 
-    def setTileColor(self, posX:int, posY:int, r:int, g:int, b:int, a:int):
+    def setTileColor(self, posX:int, posY:int, r:int, g:int, b:int, a:int) -> None:
+        """
+        Set color for a specific tile based on its position
+        """
         self._layout.itemAtPosition(posY, posX).widget().setColor(r, g, b, a)
 
 
 # ARENA BACKGROUND CLASS TO DISPLAY THE ARENA BACKGROUND
 class ArenaBg(QWidget):
+    """
+    Class for creating the background of the arena
+    """
     def __init__(self, width:int, height:int, img:str, parent: QWidget | None = ...) -> None:
         super().__init__(parent)
 
@@ -81,6 +95,7 @@ class ArenaBg(QWidget):
         self.setFixedSize(width, height)
         self.setAutoFillBackground(True)
 
+        # Background definition        
         pixmap = QPixmap(os.fspath(CURR_DIR / img))
         palette = self.palette()
         brush = QBrush(pixmap.scaled(width, height, Qt.IgnoreAspectRatio))
@@ -90,12 +105,17 @@ class ArenaBg(QWidget):
 
 # GLOBAL ARENA WIDGET
 class ArenaWidget(QWidget):
+    """
+    Class containing all the elements of the local arena: the grid and the background. It overlays the 2 elements so it can reproduce the painting effect of the players on the arena image
+    """
     def __init__(self, parent: QWidget | None = ...) -> None:
+        """
+        After setting basic values for the widget, set the layout and create both the grid and background widgets.
+        Insert the 2 widgets in the layout stacked, and define the front layout.
+        """
         super().__init__(parent)
 
         self.objectName = 'arenaWidget'
-        # mainContainer = QWidget(self, objectName = "mainContainer")
-        # layout = QStackedLayout(mainContainer, objectName = "mainLayout")
         layout = QStackedLayout(self, objectName = "mainLayout")
         self.setLayout(layout)
 
@@ -107,14 +127,10 @@ class ArenaWidget(QWidget):
         layout.setStackingMode(QStackedLayout.StackingMode.StackAll)
         layout.setCurrentIndex(1)
 
+        # Coloring the spawn areas for test
         arenaGrid.setTileColor(3, 1, 150, 150, 0, 255)
         arenaGrid.setTileColor(2, 2, 150, 150, 0, 255)
         arenaGrid.setTileColor(1, 3, 150, 150, 0, 255)
         arenaGrid.setTileColor(24, 8, 0, 0, 150, 255)
         arenaGrid.setTileColor(23, 9, 0, 0, 150, 255)
         arenaGrid.setTileColor(22, 10, 0, 0, 150, 255)
-
-        # self.setFixedSize(QWidget.minimumSize(self))
-        # self.adjustSize()
-        
-        # self.setCentralWidget(self)
